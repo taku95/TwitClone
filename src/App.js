bc7ce2fc5a04595387d009e3d6b791da900b27b4
taku95 <t.y.npng1995@gmail.com>
2023-05-20 12:40:08 +0900

    navbar追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-/* eslint-disable no-unused-vars */
-import Button from "@mui/material/Button";
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Grid } from "@mui/material";
+
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Login from "./pages/Login";
+
+const App = () => {
   return (
-    <div className="App">
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Router>
+      <Grid container sx={{ height: "100vh" }}>
+        <Grid item xs={3}>
+          <Navbar title="My Twitter App" />
+        </Grid>
+        <Grid item xs={6}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Grid>
+        <Grid item xs={3}>
+          <Sidebar />
+        </Grid>
+      </Grid>
+    </Router>
   );
-}
+};
 
 export default App;
