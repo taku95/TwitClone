b988240e53e66f75c21b8ff988e45b43d8bf8e81
taku95 <t.y.npng1995@gmail.com>
2023-05-20 17:17:01 +0900

    ページ構成変更

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Grid } from "@mui/material";
 
-// components and pages
-import Main from "./pages/Main";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import Login from "./pages/Login";
-//auth
 import { auth } from "./firebase";
+import Main from "./pages/Main";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        // ユーザーがログインしている場合
         setIsLoggedIn(true);
         setUser(user);
       } else {
-        // ユーザーがログアウトしている場合
         setIsLoggedIn(false);
+        setUser(null);
       }
     });
 
-    // コンポーネントがアンマウントされた時にunsubscribeする
     return () => unsubscribe();
   }, []);
 
-  console.log(user);
   return (
     <Router>
-      <div>
-        {isLoggedIn ? (
-          <div>ユーザーはログインしています</div>
-        ) : (
-          <div>ユーザーはログインしていません</div>
-        )}
-      </div>
       <Grid container sx={{ height: "100vh" }}>
         <Grid item xs={3}>
           <Navbar title="My Twitter App" isLoggedIn={isLoggedIn} />
         </Grid>
-
         <Grid item xs={6}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+          <Main />
         </Grid>
-        {/* <Main /> */}
-
         <Grid item xs={3} sx={{ backgroundColor: "primary.main" }}>
           <Sidebar />
         </Grid>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,24 +1,16 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Grid } from "@mui/material";
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Signup from "./Signup";
 import Login from "./Login";
 
 const Main = () => {
   return (
-    <Router>
-      <Grid item xs={6}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Grid>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
 
